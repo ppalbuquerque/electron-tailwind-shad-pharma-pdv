db67264d0f56e8098e918b4f0efb60659aaef20e
ppalbuquerque <albpedro458@gmail.com>
2025-11-18 11:44:26 -0300

    initial commit

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+
+function App(): React.JSX.Element {
+  return (
+    <div>
+      <Button variant={'ghost'}>Click me</Button>
+    </div>
+  )
+}
+
+export default App
