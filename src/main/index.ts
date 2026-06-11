63906c4cd797e83bf211651c56a9948082fd8469
ppalbuquerque <albpedro458@gmail.com>
2026-06-11 17:34:44 -0300

    feat(app): abrir aplicação em tela cheia por padrão

--- src/main/index.ts
+++ src/main/index.ts
@@ -9,6 +9,7 @@ function createWindow(): void {
     width: 900,
     height: 670,
     show: false,
+    fullscreen: true,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
