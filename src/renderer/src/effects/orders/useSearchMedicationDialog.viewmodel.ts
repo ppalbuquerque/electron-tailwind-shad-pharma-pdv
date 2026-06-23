2b8d9de8aba98e5463d12abb7f338f6b0587c0ca
ppalbuquerque <albpedro458@gmail.com>
2026-06-23 17:30:05 -0300

    feat(orders): auto-focar busca e ativar escopo de tabela no dialog

--- src/renderer/src/effects/orders/useSearchMedicationDialog.viewmodel.ts
+++ src/renderer/src/effects/orders/useSearchMedicationDialog.viewmodel.ts
@@ -1,11 +1,14 @@
 import { useEffect } from 'react'
 import { useForm, UseFormRegister } from 'react-hook-form'
 import { useQuery } from '@tanstack/react-query'
+import { useHotkeysContext } from 'react-hotkeys-hook'
 
 import { MEDICATION_QUERY_KEYS } from '@/services/medication/medication.query.keys'
 import { MedicationService, SearchResponse } from '@/services/medication/medication.service'
 
 import { useDebounce } from '@/hooks/use-debounce'
+
+import { HotkeyScope } from '@/lib/hotkey-scopes'
 
 interface SearchMedicationForm {
   medicationName: string
@@ -15,12 +18,14 @@ interface SearchMedicationDialogViewModel {
   register: UseFormRegister<SearchMedicationForm>
   isLoadingMedications: boolean
   medicationTableData: SearchResponse
+  focusSearchInput: () => void
 }
 
 function useSearchMedicationDialogViewModel(
-  medicationName: string
+  medicationName: string,
+  open: boolean
 ): SearchMedicationDialogViewModel {
-  const { setValue, register, watch } = useForm<SearchMedicationForm>()
+  const { setValue, register, watch, setFocus } = useForm<SearchMedicationForm>()
   const watchSearchTerm = watch('medicationName', '')
 
   const debouncedSearchTerm = useDebounce(watchSearchTerm)
@@ -35,10 +40,29 @@ function useSearchMedicationDialogViewModel(
     setValue('medicationName', medicationName)
   }, [medicationName, setValue])
 
+  const { enableScope, disableScope } = useHotkeysContext()
+
+  useEffect(() => {
+    if (open) {
+      enableScope(HotkeyScope.TABLE)
+    } else {
+      disableScope(HotkeyScope.TABLE)
+    }
+
+    return () => {
+      disableScope(HotkeyScope.TABLE)
+    }
+  }, [open, enableScope, disableScope])
+
+  function focusSearchInput(): void {
+    setFocus('medicationName')
+  }
+
   return {
     register,
     medicationTableData: searchData ?? [],
-    isLoadingMedications: isLoading
+    isLoadingMedications: isLoading,
+    focusSearchInput
   }
 }
 
